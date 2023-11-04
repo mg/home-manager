255e1aa183ee4dce1cd1c33927802e73d88ebf94
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2023-11-04 16:03:44 +0000

    darwin/homebrew refactor

--- darwin/default.nix
+++ darwin/default.nix
@@ -93,37 +93,7 @@
 
   security.pam.enableSudoTouchIdAuth = true;
 
-  homebrew = {
-    enable = true;
-    caskArgs.no_quarantine = true;
-    global.brewfile = true;
-    /*masApps = {
-      "LimeChat" = 414030210;
-      "Pastebot" = 1179623856;
-      "Ivory" = 2145332318;
-      "Apple Developer" = 640199958;
-      "TestFlight" = 899247664;
-      "Transporter" = 1450874784;
-    };*/
-    brews = [ "mas" ];
-    casks = [ 
-      "db-browser-for-sqlite" 
-      "alacritty" 
-      "kitty" 
-      "wezterm" # https://wezfurlong.org/wezterm/index.html
-      "xcodes" 
-      "hammerspoon" 
-      "element"
-      "google-chrome"
-      "firefox"
-      "visual-studio-code"
-      "obsidian"
-      "netnewswire"
-      "vlc"
-      "lastpass"
-      "discord"
-    ];
-  };
+  homebrew = import ./homebrew.nix;
 
   system.defaults.CustomUserPreferences = {
     NSGlobalDomain = {
