46047ad3de150fce133fd7d21d115b4096652f43
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2024-06-25 09:00:02 +0000

    nvim toggleterm

--- home-manager/dotfiles/nvim/lua/plugins/toggleterm.lua
+++ home-manager/dotfiles/nvim/lua/plugins/toggleterm.lua
@@ -1,15 +1,16 @@
-local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-function LazygitToggle()
-	lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>lua LazygitToggle()<CR>", { noremap = true, silent = true })
-
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	opts = {--[[ things you want to change go here]]
 	},
+	config = function()
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+
+		function LazygitToggle()
+			lazygit:toggle()
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>lua LazygitToggle()<CR>", { noremap = true, silent = true })
+	end,
 }
