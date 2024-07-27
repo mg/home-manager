8394e9938b695100b9109ffc646df6cf4ed02e38
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2024-07-27 13:17:41 +0000

    markdown plugin

---
+++ home-manager/dotfiles/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,8 @@
+return {
+	"MeanderingProgrammer/markdown.nvim",
+	main = "render-markdown",
+	opts = {},
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+}
