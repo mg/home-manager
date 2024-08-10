8870a96a20857d9ace076192b565d788aac0d76f
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2024-08-10 11:29:30 +0000

    nvim cmp: tailwind integration

--- home-manager/dotfiles/nvim/lua/plugins/cmp.lua
+++ home-manager/dotfiles/nvim/lua/plugins/cmp.lua
@@ -38,7 +38,19 @@ return { -- Autocompletion
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
+		"luckasRanarison/tailwind-tools.nvim",
+		"onsails/lspkind-nvim",
 	},
+	opts = function()
+		return {
+			formatting = {
+				format = require("lspkind").cmp_format({
+					before = require("tailwind-tools.cmp").lspkind_format,
+				}),
+			},
+		}
+	end,
+
 	config = function()
 		-- See `:help cmp`
 		local cmp = require("cmp")
