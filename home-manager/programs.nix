daac0695508330e2791514555c8e1eb51c09349a
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2024-08-27 18:55:23 +0000

    tmux setup

--- home-manager/programs.nix
+++ home-manager/programs.nix
@@ -50,6 +50,29 @@
       enableNushellIntegration = true;
     };
 
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.tmux.plugins
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/misc/tmux-plugins/default.nix
+    tmux = {
+      enable = true;
+      clock24 = true;
+      mouse = true;
+      prefix = "C-a";
+      sensibleOnTop = true;
+      escapeTime = 0;
+      plugins = with pkgs; [
+        tmuxPlugins.cpu
+        tmuxPlugins.mode-indicator
+        tmuxPlugins.nord
+        tmuxPlugins.fingers
+      ];
+      extraConfig = '' 
+        # Set new panes to open in current directory
+        bind c new-window -c "#{pane_current_path}"
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+      '';
+    };
+
     # https://direnv.net/
     direnv = {
       enable = true;
