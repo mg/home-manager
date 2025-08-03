abc378842ca4b8976739abc765ac0067d0ecb901
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2025-08-03 13:02:01 +0000

    nvim: testing fff.lua

---
+++ home-manager/dotfiles/nvim/lua/plugins/_fff.lua
@@ -0,0 +1,18 @@
+-- https://github.com/dmtrKovalenko/fff.nvim
+
+return {
+	"dmtrKovalenko/fff.nvim",
+	build = "cargo build --release",
+	opts = {
+		-- pass here all the options
+	},
+	keys = {
+		{
+			"<leader>ff", -- try it if you didn't it is a banger keybinding for a picker
+			function()
+				require("fff").toggle()
+			end,
+			desc = "Toggle FFF",
+		},
+	},
+}
