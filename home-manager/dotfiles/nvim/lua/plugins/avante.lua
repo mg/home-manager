d2c2608108da880bb032624ab7db40c104260947
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2025-08-08 22:19:56 +0000

    nvim: MCPHub integration into avante/code companion

--- home-manager/dotfiles/nvim/lua/plugins/avante.lua
+++ home-manager/dotfiles/nvim/lua/plugins/avante.lua
@@ -10,6 +10,32 @@ return {
 		-- for example
 		provider = "copilot",
 	},
+	config = function()
+		require("avante").setup({
+			-- MCPHub integration
+			system_prompt = function()
+				local hub = require("mcphub").get_hub_instance()
+				return hub and hub:get_active_servers_prompt() or ""
+			end,
+			custom_tools = function()
+				return {
+					require("mcphub.extensions.avante").mcp_tool(),
+				}
+			end,
+			disabled_tools = {
+				"list_files", -- Built-in file operations
+				"search_files",
+				"read_file",
+				"create_file",
+				"rename_file",
+				"delete_file",
+				"create_dir",
+				"rename_dir",
+				"delete_dir",
+				"bash", -- Built-in terminal access
+			},
+		})
+	end,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
