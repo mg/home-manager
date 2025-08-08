c56882a3643db1f1a49c6049206ed054aac32b76
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2025-08-08 10:07:21 +0000

    nvim: trouble: disable auto_preview behaviour

--- home-manager/dotfiles/nvim/lua/plugins/trouble.lua
+++ home-manager/dotfiles/nvim/lua/plugins/trouble.lua
@@ -1,6 +1,8 @@
 return {
 	"folke/trouble.nvim",
-	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	opts = {
+		auto_preview = false,
+	},
 	cmd = "Trouble",
 	keys = {
 		{
