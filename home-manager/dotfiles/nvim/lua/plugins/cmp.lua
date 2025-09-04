2c8ae378e9a6f8b8ed3dca6c5c3be2e7cee0571d
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2025-09-04 22:27:58 +0000

    nvim: cmp: remap keys

--- home-manager/dotfiles/nvim/lua/plugins/cmp.lua
+++ home-manager/dotfiles/nvim/lua/plugins/cmp.lua
@@ -86,9 +86,9 @@ return { -- Autocompletion
 
 				-- If you prefer more traditional completion keymaps,
 				-- you can uncomment the following lines
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+				-- ["<Tab>"] = cmp.mapping.select_next_item(),
+				-- ["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
 				-- Manually trigger a completion from nvim-cmp.
 				--  Generally you don't need this, because nvim-cmp will display
