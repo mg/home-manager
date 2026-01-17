c5e8f6bf3cd695a051d96c292923bd0cddecb517
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2026-01-17 10:01:35 +0000

    nvim: review

---
+++ home-manager/dotfiles/nvim/lua/plugins/review.lua
@@ -0,0 +1,15 @@
+-- https://github.com/georgeguimaraes/review.nvim
+
+return {
+  "georgeguimaraes/review.nvim",
+  dependencies = {
+    "esmuellert/codediff.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = { "Review" },
+  -- keys = {
+  --   { "<leader>r", "<cmd>Review<cr>", desc = "Review" },
+  --   { "<leader>R", "<cmd>Review commits<cr>", desc = "Review commits" },
+  -- },
+  opts = {},
+}
