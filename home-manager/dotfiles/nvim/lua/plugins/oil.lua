44fa6a72462517f1befd78e8445fc97b4a01ae50
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2026-02-20 22:01:06 +0000

    neovim: oil: fstats

--- home-manager/dotfiles/nvim/lua/plugins/oil.lua
+++ home-manager/dotfiles/nvim/lua/plugins/oil.lua
@@ -1,24 +1,30 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({
-			default_file_explorer = true,
-			delete_to_trash = true,
-			skip_confirm_for_simple_edits = true,
-			view_options = {
-				show_hidden = true,
-				natural_order = true,
-				is_always_hidden = function(name, _)
-					return name == ".." or name == ".git"
-				end,
-			},
-			win_options = {
-				wrap = true,
-				signcolumn = "yes:2",
-			},
-		})
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	end,
+  "stevearc/oil.nvim",
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".." or name == ".git"
+        end,
+      },
+      win_options = {
+        wrap = true,
+        signcolumn = "yes:2",
+      },
+      columns = {
+        "icon",
+        "permission",
+        "size",
+        "mtime",
+      },
+    })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
 }
