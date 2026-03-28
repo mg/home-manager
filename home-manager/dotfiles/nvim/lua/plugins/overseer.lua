7bd2f6a69fdcae643c474d9369da8a2b8c47bd03
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2026-03-28 10:47:00 +0000

    neovim: overseer migration

--- home-manager/dotfiles/nvim/lua/plugins/overseer.lua
+++ home-manager/dotfiles/nvim/lua/plugins/overseer.lua
@@ -5,10 +5,28 @@ vim.keymap.set("n", "<leader>tr", ":OverseerRun<CR>", { desc = "[r]un task" })
 return {
 	"stevearc/overseer.nvim",
 	opts = {
-		strategy = {
-			"toggleterm",
-			direction = "tab",
-			auto_scroll = true,
+		component_aliases = {
+			default = {
+				"on_exit_set_status",
+				"on_complete_notify",
+				{ "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+				{ "open_output", on_start = "always", direction = "tab", focus = true },
+			},
 		},
 	},
+	config = function(_, opts)
+		require("overseer").setup(opts)
+		-- Enter terminal mode when overseer opens task output
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			callback = function(args)
+				if vim.bo[args.buf].buftype == "terminal" then
+					vim.schedule(function()
+						if vim.api.nvim_get_current_buf() == args.buf then
+							vim.cmd("startinsert")
+						end
+					end)
+				end
+			end,
+		})
+	end,
 }
