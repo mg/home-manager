5399005d4b2792823df2ea5b36bc2dd1acf8593b
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2026-04-05 18:41:09 +0000

    neovim: disable supermaven when ~/Work, enable copilot

--- home-manager/dotfiles/nvim/lua/plugins/copilot.lua
+++ home-manager/dotfiles/nvim/lua/plugins/copilot.lua
@@ -8,12 +8,26 @@ vim.api.nvim_create_autocmd("User", {
   end,
 })
 
+local function is_work_project()
+  local cwd = vim.fn.getcwd()
+  local work_dir = vim.fn.expand("~/Work")
+  return cwd:sub(1, #work_dir) == work_dir
+end
+
 return {
   "zbirenbaum/copilot.lua",
-  lazy = true,
+  cond = is_work_project(),
+  event = "BufRead",
   config = function()
     require("copilot").setup({
-      suggestion = { enabled = true },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-]>",
+          dismiss = "<C-c>",
+        },
+      },
       panel = { enabled = true },
     })
   end,
--- home-manager/dotfiles/nvim/lua/plugins/supermaven.lua
+++ home-manager/dotfiles/nvim/lua/plugins/supermaven.lua
@@ -1,5 +1,12 @@
+local function is_work_project()
+	local cwd = vim.fn.getcwd()
+	local work_dir = vim.fn.expand("~/Work")
+	return cwd:sub(1, #work_dir) == work_dir
+end
+
 return {
 	"supermaven-inc/supermaven-nvim",
+	cond = not is_work_project(),
 	event = "BufRead",
 	config = function()
 		require("supermaven-nvim").setup({
