25b2b4115c1345a2feb7e55256c3f6c4dacb59c0
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2026-04-05 18:40:34 +0000

    neovim: use homemanager folder for config folder

--- home-manager/dotfiles/nvim/lua/plugins/fff.lua
+++ home-manager/dotfiles/nvim/lua/plugins/fff.lua
@@ -1,12 +1,7 @@
 return {
   "dmtrKovalenko/fff.nvim",
-  build = function()
-    -- this will download prebuild binary or try to use existing rustup toolchain to build from source
-    -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
-    require("fff.download").download_or_build_binary()
-  end,
-  -- if you are using nixos
-  -- build = "nix run .#release",
+  tag = "v0.5.2",
+  build = "curl -sL https://github.com/dmtrKovalenko/fff.nvim/releases/download/v0.5.2/aarch64-apple-darwin.dylib -o target/release/libfff_nvim.dylib",
   opts = { -- (optional)
     debug = {
       enabled = true, -- we expect your collaboration at least during the beta
