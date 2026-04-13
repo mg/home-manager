7320b96f0b7721a1b7f9f00344cbcb49655d9db6
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2026-04-13 14:48:00 +0000

    nvim: themes

---
+++ home-manager/dotfiles/nvim/lua/packages/kanagawa.lua
@@ -0,0 +1,6 @@
+return {
+  src = "https://github.com/rebelot/kanagawa.nvim",
+  -- config = function()
+  --   vim.cmd("colorscheme tokyonight-night")
+  -- end,
+}
---
+++ home-manager/dotfiles/nvim/lua/packages/rose-pine.lua
@@ -0,0 +1,6 @@
+return {
+  src = "https://github.com/rose-pine/neovim",
+  -- config = function()
+  --   vim.cmd("colorscheme tokyonight-night")
+  -- end,
+}
