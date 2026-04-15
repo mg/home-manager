bcbc06e37926906f8a8656cd1a4758163ef5b2a5
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2026-04-15 20:22:42 +0000

    nvim: flutter & elixir

--- home-manager/dotfiles/nvim/lua/lib.lua
+++ home-manager/dotfiles/nvim/lua/lib.lua
@@ -14,4 +14,16 @@ function M.is_work_dir()
   return cwd:sub(1, #work_dir) == work_dir
 end
 
+--- Check if the current buffer is inside an Elixir project.
+---@return boolean
+function M.is_elixir_project()
+  return vim.fs.root(0, 'mix.exs') ~= nil
+end
+
+--- Check if the current buffer is inside a Flutter/Dart project.
+---@return boolean
+function M.is_flutter_project()
+  return vim.fs.root(0, 'pubspec.yaml') ~= nil
+end
+
 return M
---
+++ home-manager/dotfiles/nvim/lua/plugins/flutter-tools.lua
@@ -0,0 +1,70 @@
+return {
+  "akinsho/flutter-tools.nvim",
+  cond = require("lib").is_flutter_project,
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim",
+  },
+  keys = {
+    {
+      "<leader>F",
+      function()
+        local commands = {
+          { name = "FlutterRun",              desc = "Run the current project" },
+          { name = "FlutterDebug",            desc = "Force run in debug mode" },
+          { name = "FlutterDevices",          desc = "List connected devices" },
+          { name = "FlutterEmulators",        desc = "List emulators" },
+          { name = "FlutterReload",           desc = "Reload the running project" },
+          { name = "FlutterRestart",          desc = "Restart the current project" },
+          { name = "FlutterQuit",             desc = "End running session" },
+          { name = "FlutterAttach",           desc = "Attach to a running app" },
+          { name = "FlutterDetach",           desc = "Detach but keep process running" },
+          { name = "FlutterOutlineToggle",    desc = "Toggle widget tree outline" },
+          { name = "FlutterOutlineOpen",      desc = "Open widget tree outline" },
+          { name = "FlutterDevTools",         desc = "Start Dart Dev Tools server" },
+          { name = "FlutterDevToolsActivate",  desc = "Activate Dart Dev Tools server" },
+          { name = "FlutterCopyProfilerUrl",  desc = "Copy profiler URL to clipboard" },
+          { name = "FlutterLspRestart",       desc = "Restart Dart language server" },
+          { name = "FlutterSuper",            desc = "Go to super class/method" },
+          { name = "FlutterReanalyze",        desc = "Force LSP reanalyze" },
+          { name = "FlutterRename",           desc = "Rename and update imports" },
+          { name = "FlutterLogClear",         desc = "Clear log buffer" },
+          { name = "FlutterLogToggle",        desc = "Toggle log buffer" },
+        }
+
+        Snacks.picker({
+          title = "Flutter",
+          items = vim.tbl_map(function(cmd)
+            return { text = cmd.name .. " - " .. cmd.desc, cmd = cmd.name }
+          end, commands),
+          format = function(item)
+            return { { item.text } }
+          end,
+          confirm = function(picker, item)
+            picker:close()
+            vim.cmd(item.cmd)
+          end,
+        })
+      end,
+      desc = "Flutter commands",
+    },
+  },
+  config = function()
+    require("flutter-tools").setup({
+      lsp = {
+        color = {
+          enabled = true,
+        },
+      },
+      dev_tools = {
+        autostart = false,
+        auto_open_browser = false,
+      },
+      outline = {
+        open_cmd = "30vnew",
+        auto_open = false,
+      },
+    })
+  end,
+}
