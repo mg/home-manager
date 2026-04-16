95d1816148a0f17fae0e81bbe6ba8a34bc522040
Magnús Ö. Gylfason <Magnus.O.Gylfason@landsbankinn.is>
2026-04-16 09:30:36 +0000

    nvim: tabout

---
+++ home-manager/dotfiles/nvim/lua/plugins/tabout.lua
@@ -0,0 +1,26 @@
+return {
+  "abecodes/tabout.nvim",
+  event = "InsertCharPre",
+  config = function()
+    require("tabout").setup({
+      tabkey = "<Tab>",
+      backwards_tabkey = "<S-Tab>",
+      act_as_tab = true,
+      act_as_shift_tab = false,
+      default_tab = "<C-t>",
+      default_shift_tab = "<C-d>",
+      enable_backwards = true,
+      completion = false,
+      tabouts = {
+        { open = "'", close = "'" },
+        { open = '"', close = '"' },
+        { open = "`", close = "`" },
+        { open = "(", close = ")" },
+        { open = "[", close = "]" },
+        { open = "{", close = "}" },
+      },
+      ignore_beginning = true,
+      exclude = {},
+    })
+  end,
+}
